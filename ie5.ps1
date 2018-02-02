0cb06f3f5077858a3f3c386aef175b618a10a482
Ricky Raghavan <rajivsr@yahoo.com>
2018-02-02 16:51:37 -0500

    Code to get the day of the year

---
+++ ie5.ps1
@@ -0,0 +1,8 @@
+﻿#Drop Down on Harvest-Gets the Week Preceeding the Previous Week
+
+$ie = New-Object -ComObject internetexplorer.application
+$ie.Visible = $true
+$date=Get-Date -UFormat %j
+$date_twoWeeks=$date-14
+
+$ie.Navigate("https://sohodragon.harvestapp.com/missing_time/period/$date_twoWeeks/2018")
